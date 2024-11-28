5e85ad0a7c844e4f883f54b42c19527424073362
m-usamasaleem <21100273@lums.edu.pk>
2024-11-28 18:33:39 -0500

    Trigger redeployment

--- git.sh
+++ git.sh
@@ -1,22 +1,3 @@
-#!/bin/bash
-
-# Ensure the main branch exists and switch to it
-if git rev-parse --verify main >/dev/null 2>&1; then
-    echo "Switching to the 'main' branch."
-    git checkout main
-else
-    echo "Creating and switching to the 'main' branch."
-    git checkout -b main origin/main
-fi
-
-# Stage all changes
-echo "Staging changes."
 git add .
-
-# Commit changes
-echo "Committing changes."
-git commit -m "Updated site content"
-
-# Push changes to the main branch
-echo "Pushing changes to 'origin/main'."
+git commit -m "Trigger redeployment"
 git push origin main
